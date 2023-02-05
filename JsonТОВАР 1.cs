a81a002fadae064e825ab75a56c6b52eb657ccaa
Maris Shapiro <19maris72@gmail.com>
2023-02-05 23:44:47 +0300

    Добавьте файлы проекта.

---
+++ JsonТОВАР 1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_16_СамРабота_с_JSON_товар
+{
+    class JsonТОВАР_1
+    {
+    }
+}
